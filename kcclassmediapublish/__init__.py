bac9ee34a8f8b7ee33e30d4999ea97ee042e16e9
Marko Novak <marko@fikret.(none)>
2012-10-08 16:45:57 +0200

    Implemented the login and the media listing.

--- kcclassmediapublish/__init__.py
+++ kcclassmediapublish/__init__.py
@@ -1,15 +1,18 @@
 from pyramid.config import Configurator
+from pyramid.session import UnencryptedCookieSessionFactoryConfig
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(settings=settings)
+    my_session_factory = UnencryptedCookieSessionFactoryConfig('KCClassM3dIaPublishS3cr3t')
+    config = Configurator(settings=settings, session_factory=my_session_factory)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('login', '/login/{provider}')
-    config.add_route('login_response', '/login_response/{provider}')
-    config.add_route('list-media', '/media-list/{provider}')
-    config.add_route('upload-media-show', '/media-upload')
-    config.add_route('upload-media-confirm', '/upload-media-confirm')
+    config.add_route('login_response', '/login_response')
+    config.add_route('list_media', '/media-list/{provider}')
+    config.add_route('upload_media_show', '/media-upload')
+    config.add_route('upload_media_confirm', '/upload-media-confirm')
+    config.add_route('delete_media', '/media-delete/{provider}/{id}')
     config.scan()
     return config.make_wsgi_app()
--- kcclassmediapublish/views.py
+++ kcclassmediapublish/views.py
@@ -2,8 +2,11 @@ import logging
 from pyramid.view import view_config
 from pyramid.response import Response
 from kcclassmediapublish.upload.upload_file import get_file_from_user
-from pyramid.decorator import reify
 from pyramid.renderers import get_renderer
+from kcclassmediapublish import service_creator
+from pyramid.httpexceptions import HTTPFound
+from pyramid.url import route_url
+from gdata.service import BadAuthentication
 
 log = logging.getLogger( __name__ )
 
@@ -19,7 +22,7 @@ SERVICES_MENU = {
 }
 
 def get_global_template():
-    return get_renderer("templates/global_layout.pt").implementation()
+    return get_renderer('templates/global_layout.pt').implementation()
 
 def get_services_menu(request):
     new_menu = SERVICES_MENU.copy()
@@ -27,6 +30,14 @@ def get_services_menu(request):
     for menu_key in new_menu:
         new_menu[menu_key]['current'] = url.endswith(new_menu[menu_key]['href'])
     return new_menu
+
+def get_service(request, provider):
+    session = request.session
+    service_key = get_service_key(provider)
+    return session.get(service_key, None)
+
+def get_service_key(provider):
+    return 'service_' + provider
 
 def add_global_template_data(dct, request):
     dct['main'] = get_global_template()
@@ -37,69 +48,101 @@ def add_global_template_data(dct, request):
 def home(request):
     return add_global_template_data({}, request)
 
-@view_config(route_name='login', renderer='templates/login.pt')
+@view_config(route_name='login', renderer='templates/login.pt', request_method='GET')
 def login(request):
     provider = request.matchdict['provider']
     return add_global_template_data({'provider': provider}, request)
 
-@view_config(route_name='login_response', renderer='templates/home.pt')
+@view_config(route_name='login_response', renderer='templates/login.pt', request_method='POST')
 def login_response(request):
-    controls = request.POST.items()
-    return add_global_template_data({}, request)
+    provider = request.params['provider']
+    username = request.params['inputUsername']
+    password = request.params['inputPassword']
+    try:
+        service = service_creator.create(provider, username, password)
+        if service is not None:
+            session = request.session
+            service_key = get_service_key(provider)
+            session[service_key] = service
+            return HTTPFound(route_url('list_media', request, provider=provider))
+    except BadAuthentication, e:
+        error = 'Incorrect username or password.'
+    except Exception, e:
+        error = 'Login failed.'
+        log.error('Service creation failed: ' + str(e))
+    return add_global_template_data({'error_msg': error, 'provider': provider}, request)
 #    try:
 #        appstruct = myform.validate(controls)
 #    except ValidationFailure, e:
 #        return {'form':e.render(), 'values': False}
 #    # Process the valid form data, do some work
 #    values = {
-#        "name": appstruct['name'],
-#        "shoe_size": appstruct['shoe_size'],
+#        'name': appstruct['name'],
+#        'shoe_size': appstruct['shoe_size'],
 #        }
-#    return {"form": myform.render(), "values": values}
+#    return {'form': myform.render(), 'values': values}
     
 #@view_config(route_name='home', renderer='templates/media_list.pt')
-@view_config(route_name='list-media', renderer='templates/media_list.pt')
+@view_config(route_name='list_media', renderer='templates/media_list.pt')
 def list_media(request):
     provider = request.matchdict.get('provider', 'youtube')
-    log.debug("Listing media: provider=%s" % provider)
-    service = service_creator.create(provider, username, password)
-    return {}
+    service = get_service(request, provider)
+    if service is None:
+        return HTTPFound(route_url('login', request, provider=provider))
+    log.debug('Listing media: provider=%s' % provider)
+    media_list = service.list()
+    return add_global_template_data({'provider': provider, 'media_list': media_list}, request)
 
-@view_config(route_name='upload-media-show', renderer='templates/media_upload.pt')
+@view_config(route_name='upload_media_show', renderer='templates/media_upload.pt')
 def upload_media_show(request):
-    log.debug("Displaying uploading form.")
-    return {}
+    log.debug('Displaying uploading form.')
+    return add_global_template_data({}, request)
 
-@view_config(route_name='upload-media-confirm')
+@view_config(route_name='upload_media_confirm')
 def upload_file(request):
     # ``filename`` contains the name of the file in string format.
     #
     # WARNING: this example does not deal with the fact that IE sends an
     # absolute file *path* as the filename.  This example is naive; it
     # trusts user input.
-    log.debug("Uploading file.")
+    log.debug('Uploading file.')
     filename = request.POST['filepath'].filename
-    log.debug("filename: " + filename);
+    log.debug('filename: ' + filename);
     # 'filepath' contains the actual file data which needs to be
     # stored somewhere.
     input_file = request.POST['filepath'].file
     out_filepath = get_file_from_user(filename, input_file)
-    log.debug("Output filepath: %s" % out_filepath)
+    log.debug('Output filepath: %s' % out_filepath)
     # get media metadata.
     
     # upload the media file to the selected media sites.
     media_site = request.POST['media-site']
-    log.debug("Media site: %s" % media_site)
-    if media_site == "youtube":
+    log.debug('Media site: %s' % media_site)
+    if media_site == 'youtube':
         pass
-    elif media_site == "picasaweb":
+    elif media_site == 'picasaweb':
         pass
-    elif media_site == "slideshare":
+    elif media_site == 'slideshare':
         pass
-    elif media_site == "flickr":
+    elif media_site == 'flickr':
         pass
     else:
-        log.error("Unidentified media site: " + media_site)        
-    
-    log.debug("Uploading succeeded.")
+        log.error('Unidentified media site: ' + media_site)
+    log.debug('Uploading succeeded.')
     return Response('OK')
+
+@view_config(route_name='delete_media')
+def delete_media(request):
+    provider = request.matchdict.get('provider', 'youtube')
+    media_id = request.matchdict.get('id', None)
+    service = get_service(request, provider)
+    if service is None:
+        return HTTPFound(route_url('login', request, provider=provider))
+    log.debug('Deleting media: provider=%s, id=%s' % (provider, media_id))
+    try:
+        service.unpublish(media_id);
+    except Exception, e:
+        log.error('Unpublishing failed: ' + str(e))
+    media_list = service.list()
+    
+    return add_global_template_data({'provider': provider, 'media_list': media_list}, request)
